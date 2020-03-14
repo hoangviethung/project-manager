c138bce7f6630fe5579c7bc3584c045e7ae0eadd
hoangviethung <hoangviet.hung.009@gmail.com>
2020-03-15 02:31:22 +0700

    DONE INDEX 4 ONLY PC

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -7,7 +7,82 @@ const checkLayoutBanner = () => {
 	$('main').css('padding-top', heightHeader);
 }
 
+const sliderCustomer = () => {
+	var thumbnail = new Swiper('.slider-customer .thumbnail-image .swiper-container', {
+		spaceBetween: 10,
+		slidesPerView: 4,
+		loop: true,
+		observer: true,
+		observeParents: true,
+		slideToClickedSlide: true,
+		speed: 1000,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		navigation: {
+			nextEl: '.thumbnail-image .swiper-button-next',
+			prevEl: '.thumbnail-image .swiper-button-prev',
+		},
+	});
+
+	var review = new Swiper('.slider-customer .review-image .swiper-container', {
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		speed: 1000,
+		spaceBetween: 20,
+		loop: true,
+		simulateTouch: false,
+		slidesPerView: 1,
+		thumbs: {
+			swiper: thumbnail,
+		},
+		navigation: {
+			nextEl: '.thumbnail-image .swiper-button-next',
+			prevEl: '.thumbnail-image .swiper-button-prev',
+		}
+	});
+}
+
+const setHeightOverFolowBySomeElement = (selector) => {
+	$(selector).each(function() {
+		const heightGet = $(this).find('[data-getHeight]').innerHeight();
+		const heightSet = $(this).find('[data-setHeight]');
+		const responsive = heightSet.attr('data-setHeight');
+		if (window.innerWidth > responsive) {
+			heightSet.css('max-height', heightGet)
+		}
+	})
+}
+
+const marginRightThumbnailSliderCustomer = () => {
+	const width = $('.slider-customer .thumbnail-image .swiper-button-next').outerWidth();
+	console.log(width);
+	$('.slider-customer .thumbnail-image .swiper-container').css('margin-right', width + 19);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	Loading();
+	Loading().then(() => {
+		// GET HEIGHT SOMWE ELEMENT
+		setHeightOverFolowBySomeElement('.thumbnail-image,.review-image');
+	});
+	// WOW JS
+	new WOW().init();
+	// CHECK BANNER
 	checkLayoutBanner();
+	// SLIDER ĐỐI TÁC
+	sliderCustomer();
+	// MARGIN
+	marginRightThumbnailSliderCustomer();
 });
+
+document.addEventListener('resize', () => {
+	setHeightOverFolowBySomeElement('.thumbnail-image,.review-image');
+	marginRightThumbnailSliderCustomer();
+})
